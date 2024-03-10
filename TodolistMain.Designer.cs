53a20f1886a054698754807e4cd3db273a63422f
briana <briana.seo90@gmail.com>
2024-03-10 20:50:28 +0900

    first commit. 현재 file에서 todo list를 읽어와 data grid view에 출력하도록 기본기능 구현

---
+++ TodolistMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Todolist
+{
+    partial class TodolistMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_load_todolist = new System.Windows.Forms.Button();
+            this.todolist_grid_view = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.todolist_grid_view)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_load_todolist
+            // 
+            this.btn_load_todolist.Location = new System.Drawing.Point(12, 415);
+            this.btn_load_todolist.Name = "btn_load_todolist";
+            this.btn_load_todolist.Size = new System.Drawing.Size(181, 23);
+            this.btn_load_todolist.TabIndex = 0;
+            this.btn_load_todolist.Text = "Load Todo List";
+            this.btn_load_todolist.UseVisualStyleBackColor = true;
+            this.btn_load_todolist.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // todolist_grid_view
+            // 
+            this.todolist_grid_view.AllowUserToAddRows = false;
+            this.todolist_grid_view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.todolist_grid_view.Location = new System.Drawing.Point(12, 24);
+            this.todolist_grid_view.Name = "todolist_grid_view";
+            this.todolist_grid_view.RowTemplate.Height = 23;
+            this.todolist_grid_view.Size = new System.Drawing.Size(776, 385);
+            this.todolist_grid_view.TabIndex = 1;
+            this.todolist_grid_view.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.todolist_grid_view_CellContentClick);
+            // 
+            // TodolistMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.todolist_grid_view);
+            this.Controls.Add(this.btn_load_todolist);
+            this.Name = "TodolistMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.todolist_grid_view)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_load_todolist;
+        private System.Windows.Forms.DataGridView todolist_grid_view;
+    }
+}
+
---
+++ TodolistMain.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Todolist
+{
+
+    public partial class TodolistMain : Form
+    {
+        public TodolistMain()
+        {
+            InitializeComponent();
+
+            todolist_grid_view.Columns.Clear();
+
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            checkBoxColumn.HeaderText = "Finished";
+            checkBoxColumn.Name = "checkBoxColumn";
+
+            todolist_grid_view.RowHeadersVisible = false;
+
+            todolist_grid_view.Columns.Add(checkBoxColumn);
+            todolist_grid_view.Columns.Add("todo", "To do");
+
+            List<Todo> todoList = new List<Todo>();
+            loadTodoListFromFile(todoList, "..\\..\\data\\todolist_data.txt");
+        }
+
+        void loadTodoListFromFile(List<Todo> todoList, string filePath)
+        {
+            try
+            {
+                // 파일에서 한 줄씩 읽어와서 파싱하여 Todo 객체 생성
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split('/');
+                        if (parts.Length == 2)
+                        {
+                            string todoItem = parts[0];
+                            bool finished;
+                            bool.TryParse(parts[1], out finished);
+                            Todo todo = new Todo(todoItem, finished);
+                            todoList.Add(todo);
+
+                            /*
+                             finished 여부 대신, 체크박스를 넣음.
+                             */
+                        }
+                    }
+                }
+
+                // DataGridView에 데이터 출력
+                foreach (Todo todo in todoList)
+                {
+                    // DataGridView에 행 추가
+                    int rowIndex = todolist_grid_view.Rows.Add();
+                    // DataGridView의 체크박스에 값 설정
+                    todolist_grid_view.Rows[rowIndex].Cells["checkBoxColumn"].Value = todo.Finished;
+                    // DataGridView의 할 일 컬럼에 값 설정
+                    todolist_grid_view.Rows[rowIndex].Cells["todo"].Value = todo.TodoItem;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        /*
+         * function: button1_click
+            버튼을 누르면 todolist가 기록된 파일을 읽어와 표출한다.
+            추후 db방식으로 변경.
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //btn_load_todolist.Text = "clicked";
+
+            
+
+        }
+
+        private void todolist_grid_view_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+
+    class Todo
+    {
+        public string TodoItem { get; set; }
+        public bool Finished { get; set; }
+
+        public Todo(string todoItem, bool finished)
+        {
+            TodoItem = todoItem;
+            Finished = finished;
+        }
+    }
+
+}
